32f76158010c06f5f97dac110cfe7d777a1c0dfe
gitzub <142504263+gitzub@users.noreply.github.com>
2025-02-02 22:16:32 +0900

    Update websocket_server.py

--- websocket_server.py
+++ websocket_server.py
@@ -1,13 +1,22 @@
+# from flask import Flask
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# socketio = SocketIO(
+#     app, 
+#     cors_allowed_origins="*",
+#     async_mode="eventlet",
+#     transports=["websocket"]
+# )
+
+# if __name__ == '__main__':
+#     socketio.run(app, host="0.0.0.0", port=5000)
+
+
 from flask import Flask
-from flask_socketio import SocketIO
+from socketio_instance import socketio  # 동일한 SocketIO 객체 불러오기
 
 app = Flask(__name__)
-socketio = SocketIO(
-    app, 
-    cors_allowed_origins="*",
-    async_mode="eventlet",
-    transports=["websocket"]
-)
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000)
