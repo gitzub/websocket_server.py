8e55511418e17b28bcfde54a3d6f88ff36a6d7fa
gitzub <142504263+gitzub@users.noreply.github.com>
2025-02-03 22:24:04 +0900

    Update websocket_server.py

--- websocket_server.py
+++ websocket_server.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 # SocketIO 객체를 직접 생성 (이전에는 socketio_instance.py에서 가져옴)
 socketio = SocketIO(
     app,
-    # cors_allowed_origins="https://websocketserverpy-production.up.railway.app",  # 모든 오리진 허용
+    # cors_allowed_origins="http://websocketserverpy-production.up.railway.app",  # 모든 오리진 허용
     # cors_allowed_origins="*",  # 모든 오리진 허용
     cors_allowed_origins="https://websocketserverpy-production.up.railway.app/socket.io/",
     transports=["websocket"]  # WebSocket 전송 방식 사용
